6b8d1c2ce724bec239b2f0e86b912a660155f811
Calum Gould <calumgould26@gmail.com>
2025-12-24 15:39:31 +0000

    chore: remove comments

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -1,24 +1,20 @@
-// Colors
 export const COLORS = {
   BACKGROUND_DEFAULT: '#222222',
   STROKE: '#8b7355',
   TEXT: '#ffffff',
 } as const
 
-// Board dimensions
 export const BOARD_DIMENSIONS = {
   WIDTH: 2400,
   HEIGHT: 1600,
 } as const
 
-// Text properties
 export const TEXT_PROPERTIES = {
   FONT_SIZE: 48,
   FONT_FAMILY: 'Signika',
   ALPHA: 1.0,
 } as const
 
-// Drawing properties
 export const DRAWING_PROPERTIES = {
   FILL_ALPHA: 1.0,
   STROKE_ALPHA: 1.0,
--- src/scripts/module.ts
+++ src/scripts/module.ts
@@ -2,11 +2,9 @@ import { registerSettings } from './settings'
 import { registerControls } from './controls'
 
 Hooks.once('init', () => {
-  console.log('Investigation Board module initializing...')
   registerSettings()
 })
 
 Hooks.once('ready', () => {
-  console.log('Investigation Board module is ready!')
   registerControls()
 })
